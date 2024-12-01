dfde71f293f3d19cc08a39e76a35f7e48fa3f11c
Sushant883 <panditsushant11@gmail.com>
2024-12-01 21:13:34 +0530

    Adding the typed js in js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
  // JavaScript to update the progress bar on scroll
+
+ 
  window.onscroll = function () {
     let scrollTop = document.documentElement.scrollTop; // Current vertical position
     let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight; // Total scrollable height
     let scrollPercentage = (scrollTop / scrollHeight) * 100; // Percentage scrolled
     document.getElementById("progress-bar").style.width = scrollPercentage + "%"; // Set progress bar width
   };
+
+
